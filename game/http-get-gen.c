0090e505549ac1f2d1891da1f528705f41811994
8-BIT-DEV <44956905+8-BIT-DEV@users.noreply.github.com>
2021-05-01 15:01:13 -0400

    patched version that hopefully fixes windows

--- game/http-get-gen.c
+++ game/http-get-gen.c
@@ -4,9 +4,11 @@
 #include <http.h>
 #include <matoya.h>
 
+extern bool exitnow;
+
 void http_get_free(http_get_response_t* res)
 {
-    free(res->data-res->hsize);
+    MTY_Free(res->data);
     free(res);
 }
 
@@ -16,8 +18,15 @@ http_get_response_t* http_get(const char* url)
     char path[256];
     MTY_HttpParseUrl(url, host, 256, path, 256);
     http_get_response_t* res = malloc(sizeof(http_get_response_t));
-    res->size = httpGET(host, 80, path, &res->data);
-    res->hsize = getHeaderLength(res->data) + 4;
-    res->data = res->data + res->hsize;
+    //res->size = httpGET(host, 80, path, &res->data);
+    int stat = 0;
+    bool err = MTY_HttpRequest(host, 80, false, "GET", path, "", "", 0, 5000, &res->data, &res->size, &stat);
+    //res->hsize = getHeaderLength(res->data) + 4;
+    //res->data = res->data + res->hsize;
+    if(stat != 200)
+    {
+        printf("ERR:%s\n", MTY_GetLog());
+        exitnow = true;
+    }
     return res;
 }
