3cf44dce6dd118405b6a3790363de4f6bd1663e2
8-BIT-DEV <44956905+8-BIT-DEV@users.noreply.github.com>
2021-05-01 01:46:02 -0400

    the games resources

---
+++ res/maps/default_slm_v2.tsx
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="default_slm_v2" tilewidth="32" tileheight="32" tilecount="23" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="../textures/default/player_spawn.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="../textures/default/sand.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../textures/default/sandstone.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="../textures/default/test_mat.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="../textures/default/wood.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="../textures/default/rubber.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="../textures/default/plastic.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="../textures/default/pattern1.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="../textures/default/pattern2.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="../textures/default/pattern3.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="../textures/default/lava.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="../textures/default/iron.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="../textures/default/granite.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="../textures/default/gold.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="../textures/default/glass.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="../textures/default/dirt.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="../textures/default/diamond.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="../textures/default/concrete.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="../textures/default/clay.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="../textures/default/bricks.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="../textures/default/blood_rock.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="../textures/default/bedrock.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="../textures/default/plaster.png"/>
+ </tile>
+</tileset>
